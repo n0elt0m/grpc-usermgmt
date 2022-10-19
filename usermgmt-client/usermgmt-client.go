4cd40328367c05cae88671b7876e1e436f8794d4
n0elt0m <noeltom15@gmail.com>
2022-10-19 14:35:42 +0530

    get user

--- usermgmt-client/usermgmt-client.go
+++ usermgmt-client/usermgmt-client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	pb "github.com/noel/grpc-usermgmt/usermgmt"
 	"google.golang.org/grpc"
 	"log"
@@ -13,27 +14,35 @@ const (
 )
 
 func main() {
+
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("Did not connect : %v", err)
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := pb.NewUserManagementClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-
-	var new_users = make(map[string]int32)
-	new_users["Toni"] = 25
-	new_users["Jhon"] = 29
+	var new_users = make(map[string]int)
+	new_users["Alice"] = 43
+	new_users["Bob"] = 30
 	for name, age := range new_users {
-		r, err := c.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: age})
+		r, err := c.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: int32(age)})
 		if err != nil {
-			log.Fatalf("could not create user : %v", err)
+			log.Fatalf("could not create user: %v", err)
 		}
 		log.Printf(`User Details:
 NAME: %s
 AGE: %d
 ID: %d`, r.GetName(), r.GetAge(), r.GetId())
+
 	}
+	params := &pb.GetUsersParams{}
+	r, err := c.GetUsers(ctx, params)
+	if err != nil {
+		log.Fatalf("could not create user: %v", err)
+	}
+	log.Print("\nUSER LIST: \n")
+	fmt.Printf("r.GetUsers(): %v\n", r.GetUsers())
 }
